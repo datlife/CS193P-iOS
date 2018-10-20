651a8fc0bb1e6ba053d3f097316af9cf9ca6f35d
Dat Nguyen <tdat.nguyen93@gmail.com>
2018-10-20 14:43:14 -0700

    Added Lecture 1

---
+++ lecture1_intro/Concentration/Concentration/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Concentration
+//
+//  Created by Dat on 10/20/18.
+//  Copyright © 2018 Dat Nguyen. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
